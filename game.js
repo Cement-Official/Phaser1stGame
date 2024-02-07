69044d5d9f700394acd0e41fe657af25e8715c37
Cement-Official <nmihalskiy007@gmail.com>
2024-02-07 12:12:23 +0200

    частина 8
    додає зірочки і фізику для них

--- game.js
+++ game.js
@@ -68,6 +68,20 @@ function create() {
   });
   this.physics.add.collider(player, platforms); //робить так, щоб наш гравець не падав в текстури
   cursors = this.input.keyboard.createCursorKeys();
+  stars = this.physics.add.group({
+    key: "star",
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 },
+  });
+
+  stars.children.iterate(function (child) {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+  this.physics.add.collider(stars, platforms);
+  this.physics.add.overlap(player, stars, collectStar, null, this);
+  function collectStar(player, star) {
+    star.disableBody(true, true);
+  }
 } //додає спрайти безпосередньо в гру
 
 function update() {
