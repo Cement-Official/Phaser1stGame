e8d082235a14137e81d1f56f17ef064b2caea472
Cement-Official <nmihalskiy007@gmail.com>
2024-02-07 11:41:48 +0200

    Частина друга
    додає фон

--- game.js
+++ game.js
@@ -11,8 +11,19 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+    this.load.image("sky", "assets/sky.png");
+    this.load.image("ground", "assets/platform.png");
+    this.load.image("star", "assets/star.png");
+    this.load.image("bomb", "assets/bomb.png");
+    this.load.spritesheet("dude", "assets/dude.png",
+        { frameWidth: 32, frameHeight: 48, }
+    );
+} //функція preload додає картинки (поки не додає безпосередньо в гру)
 
-function create() {}
+function create() {
+    this.add.image(400, 300, "sky");
+    this.add.image(400, 300, "star");
+} //додає картинки в гру
 
 function update() {}
