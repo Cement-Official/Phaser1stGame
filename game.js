00b1373458f04acf9ac9ee8b960c493c8d88c3e8
Cement-Official <nmihalskiy007@gmail.com>
2024-02-07 12:02:49 +0200

    частина 5
    Додали спрайт гравця

--- game.js
+++ game.js
@@ -2,22 +2,23 @@ var config = {
   type: Phaser.AUTO, // AUTO - оптимальний вибір для рендерингу бо він автоматично намагається використовувати WebGL, але якщо браузер або пристрій не підтримує його, він повертається до Canvas
   width: 800,
   height: 600, //можна ставити будь-який розмір, але в туторіалі вибраний саме цей
-   physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 300 },
-            debug: false
-        }
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
     },
-    scene: {
-        preload: preload,
-        create: create,
-        update: update
-    }
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update,
+  },
 };
 
 var game = new Phaser.Game(config);
 var platforms;
+var player;
 
 function preload() {
   this.load.image("sky", "assets/sky.png");
@@ -35,11 +36,36 @@ function create() {
 
   platforms = this.physics.add.staticGroup();
 
-  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  platforms.create(400, 568, "ground").setScale(2).refreshBody();
 
-  platforms.create(600, 400, 'ground');
-  platforms.create(50, 250, 'ground');
-  platforms.create(750, 220, 'ground');
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
+
+  player = this.physics.add.sprite(100, 450, "dude"); //додає нашого гравця
+
+  player.setBounce(0.2);
+  player.setCollideWorldBounds(true);
+
+  this.anims.create({
+    key: "left",
+    frames: this.anims.generateFrameNumbers("dude", { start: 0, end: 3 }),
+    frameRate: 10,
+    repeat: -1,
+  });
+
+  this.anims.create({
+    key: "turn",
+    frames: [{ key: "dude", frame: 4 }],
+    frameRate: 20,
+  });
+
+  this.anims.create({
+    key: "right",
+    frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }),
+    frameRate: 10,
+    repeat: -1,
+  });
 } //додає спрайти безпосередньо в гру
 
 function update() {
