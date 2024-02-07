369d09bc4a16766775a5e3f43770f269a9663e0f
Cement-Official <nmihalskiy007@gmail.com>
2024-02-07 11:55:58 +0200

    Частина треттє
    Покращили сценку для гри

--- game.js
+++ game.js
@@ -2,28 +2,46 @@ var config = {
   type: Phaser.AUTO, // AUTO - оптимальний вибір для рендерингу бо він автоматично намагається використовувати WebGL, але якщо браузер або пристрій не підтримує його, він повертається до Canvas
   width: 800,
   height: 600, //можна ставити будь-який розмір, але в туторіалі вибраний саме цей
-  scene: {
-    preload: preload,
-    create: create,
-    update: update,
-  },
+   physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
 };
 
 var game = new Phaser.Game(config);
+var platforms;
 
 function preload() {
-    this.load.image("sky", "assets/sky.png");
-    this.load.image("ground", "assets/platform.png");
-    this.load.image("star", "assets/star.png");
-    this.load.image("bomb", "assets/bomb.png");
-    this.load.spritesheet("dude", "assets/dude.png",
-        { frameWidth: 32, frameHeight: 48, }
-    );
-} //функція preload додає картинки (поки не додає безпосередньо в гру)
+  this.load.image("sky", "assets/sky.png");
+  this.load.image("ground", "assets/platform.png");
+  this.load.image("star", "assets/star.png");
+  this.load.image("bomb", "assets/bomb.png");
+  this.load.spritesheet("dude", "assets/dude.png", {
+    frameWidth: 32,
+    frameHeight: 48,
+  });
+} //функція preload додає спрайти (поки не додає безпосередньо в гру)
 
 function create() {
-    this.add.image(400, 300, "sky");
-    this.add.image(400, 300, "star");
-} //додає картинки в гру
+  this.add.image(400, 300, "sky"); //додає фон
 
-function update() {}
+  platforms = this.physics.add.staticGroup();
+
+  platforms.create(400, 568, "ground").setScale(2).refreshBody();
+
+  platforms.create(600, 400, "ground");
+  platforms.create(50, 250, "ground");
+  platforms.create(750, 220, "ground");
+} //додає спрайти безпосередньо в гру
+
+function update() { }
+
+
