9d3089cbcddcedb252d313b85ad26eefecc9fe3c
Cement-Official <nmihalskiy007@gmail.com>
2024-02-12 09:49:33 +0200

    оновлення

--- game.js
+++ game.js
@@ -34,7 +34,7 @@ function preload() {
 } //функція preload додає спрайти (поки не додає безпосередньо в гру)
 
 function create() {
-  this.add.image(400, 300, "sky"); //додає фон
+  this.add.image(400, 300, "sky");//додає фон
 
   platforms = this.physics.add.staticGroup();
 
@@ -88,28 +88,28 @@ function create() {
     fontSize: "32px",
     fill: "#000",
   });
-  function collectStar(player, star) {
-    star.disableBody(true, true);
+ function collectStar(player, star) {
+   star.disableBody(true, true);
 
-    score += 10;
-    scoreText.setText("Score: " + score);
+   score += 10;
+   scoreText.setText("Score: " + score);
 
-    if (stars.countActive(true) === 0) {
-      stars.children.iterate(function (child) {
-        child.enableBody(true, child.x, 0, true, true);
-      });
+   if (stars.countActive(true) === 0) {
+     stars.children.iterate(function (child) {
+       child.enableBody(true, child.x, 0, true, true);
+     });
 
-      var x =
-        player.x < 400
-          ? Phaser.Math.Between(400, 800)
-          : Phaser.Math.Between(0, 400);
+     var x =
+       player.x < 400
+         ? Phaser.Math.Between(400, 800)
+         : Phaser.Math.Between(0, 400);
 
-      var bomb = bombs.create(x, 16, "bomb");
-      bomb.setBounce(1);
-      bomb.setCollideWorldBounds(true);
-      bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
-    }
-  }
+     var bomb = bombs.create(x, 16, "bomb");
+     bomb.setBounce(1);
+     bomb.setCollideWorldBounds(true);
+     bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+   }
+ }
   bombs = this.physics.add.group();
 
   this.physics.add.collider(bombs, platforms);
